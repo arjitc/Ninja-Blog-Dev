7c39658be986ae2e9e113cc88b0520f96834c654
Arjit Chaudhary <arjit.c@gmail.com>
2012-04-19 16:21:52 +0400

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//Start configuration
+$title="Ninja Blog";
+$logo="logo.png";
+$ppp=5; //posts per page
+date_default_timezone_set("America/Monterrey");
+
+//Starts language config
+$olderposts = "View Older Posts";
+$newerposts = "View Newer Posts";
+$pagegen = "Page generated on";
+$copyright = "Copyright 2012 Neto.MX";
+$nopost = "No Posts Exist";
+//Stop modifying!
+
+echo "<title>".$title."</title>";
+?>
---
+++ css.php
@@ -0,0 +1,50 @@
+<style type="text/css">
+  body {
+    color: black;
+    background-color: white; 
+    font-family: 'Telex';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Telex-Regular'), url('https://themes.googleusercontent.com/static/fonts/telex/v1/QBUn5F7b-9Ulki7K75J1Bw.woff') format('woff');  }
+
+  div#main {
+	position:relative;
+	width: 900px;
+	left:50%;
+	margin-left:-500px;
+	padding: 10px; }
+
+  div#posts {
+	width:600px;
+	min-height:600px;
+	height:600px;
+	float:left;
+	border-right: 1px groove;
+	padding: 10px; }
+
+  div#ads {
+	position: relative;
+	left: 20px;
+	width: 170px;
+	float: left; }
+
+  div#footer {
+	height: 20px;
+	width:800px; }
+
+  div#older {
+	width: 50%;
+	float: left; }
+
+  div#newer {
+	width: 50%;
+	text-align: right;
+	float: right; }
+
+  a {
+	text-decoration: none; }
+
+  p#footer {
+	font-size:10px; }
+
+</style>
